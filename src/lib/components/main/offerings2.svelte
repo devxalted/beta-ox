890ecc638f52a03bd679dcc4c7a96c4598c14ce8
Jake L <jake@exalted.dev>
2022-03-09 15:36:55 -0700

    fixed image pathing

--- src/lib/components/main/offerings2.svelte
+++ src/lib/components/main/offerings2.svelte
@@ -10,19 +10,19 @@
     <div class="">
         <div class="grid-flow-col space-x-3  md:space-x-12 lg:space-x-20 text-center grid inline-block custom-snap-scroll" >
 
-            <div class="bg-gray-300 p-6 rounded drop-shadow-lg w-[85vw] md:w-52 lg:w-64 xl:w-72 2xl:w-96">
+            <div class="bg-gray-200 p-6 rounded drop-shadow-lg w-[85vw] md:w-52 lg:w-64 xl:w-72 2xl:w-96">
                 <img src={personalTraining} alt="Personal Training" class="rounded-lg drop-shadow-xl">
                 <h2 class="my-4 font-bold text-xl text-gray-600">In-Person Training</h2>
                 <p class="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, dolores eum id pariatur repellendus unde!</p>
             </div>
 
-            <div class="bg-gray-300 p-6 rounded drop-shadow-lg w-[85vw] md:w-52 lg:w-64 xl:w-72 2xl:w-96">
+            <div class="bg-gray-200 p-6 rounded drop-shadow-lg w-[85vw] md:w-52 lg:w-64 xl:w-72 2xl:w-96">
                 <img src={onlineTraining} alt="Online Coaching" class="rounded-lg drop-shadow-xl">
                 <h2 class="my-4 font-bold text-xl text-gray-600">Online Coaching</h2>
                 <p class="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem beatae eaque enim error magni praesentium!</p>
             </div>
 
-            <div class="bg-gray-300 p-6 rounded drop-shadow-lg w-[85vw] md:w-52 lg:w-64 xl:w-72 2xl:w-96">
+            <div class="bg-gray-200 p-6 rounded drop-shadow-lg w-[85vw] md:w-52 lg:w-64 xl:w-72 2xl:w-96">
                 <img src={nutrition} alt="Nutrition Coaching" class="rounded-lg drop-shadow-xl">
                 <h2 class="my-4 font-bold text-xl text-gray-600">Nutrition Coaching </h2>
                 <p class="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere laudantium, quia quibusdam quidem repellat?</p>
