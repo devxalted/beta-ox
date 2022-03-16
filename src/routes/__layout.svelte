a1f2043b34dc4b2730487346db2a4300c9d2255d
Jake L <jake@exalted.dev>
2022-03-16 09:50:11 -0600

    fixed twilio

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,4 @@
 <script>
 	import '../app.css';
-	// import Nav from '$lib/components/nav/main.svelte';
-	// import Footer from '$lib/components/footer/main.svelte';
 </script>
-
-<!--<Nav />-->
 <slot />
